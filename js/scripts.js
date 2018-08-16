b984d94840fcd3706c533b1a96b903db1ed6af35
Chris Crow and Skye Nguyen <chris@>
2018-08-16 11:06:34 -0700

    pass all specs.

--- js/scripts.js
+++ js/scripts.js
@@ -8,52 +8,54 @@ function converter(n) {
       roman += "M";
       n -= 1000;
     }
+    while (n >= 900) {
+      roman += "CM";
+      n -= 900;
+    }
     while (n >= 500) {
       roman += "D";
       n -= 500;
+    }
+    while (n >= 400) {
+      roman += "CD";
+      n -= 400;
     }
     while (n >= 100) {
       roman += "C";
       n -= 100;
     }
+    while (n >= 90) {
+      roman += "XC";
+      n -= 90;
+    }
     while (n >= 50) {
       roman += "L";
       n -= 50;
     }
+    while (n >= 40) {
+      roman += "XL";
+      n -= 40;
+    }
     while (n >= 10) {
-      if (n >= 40) {
-        roman += "XL";
-        n -= 40
-      } else {
       roman += "X";
       n -= 10;
-      }
     }
-    if (n === 9) {
+    while (n === 9) {
       roman += "IX";
       n -= 9;
-    } else {
-      while (n >= 5) {
-        roman += "V";
-        n -= 5;
-      }
     }
-    if (n === 4) {
+    while (n >= 5) {
+      roman += "V";
+      n -= 5;
+    }
+    while (n === 4) {
       roman += "IV";
-    } else {
-      while (n >= 1) {
-        roman += "I";
-        n -= 1;
-      }
+      n -= 4;
     }
-    // while (n[-1] === 4) {
-    //   roman += "IV";
-    //   n -= 4;
-    // }
-    // while (n >= 1) {
-    //   roman += "I";
-    //   n -= 1;
-    // }
+    while (n >= 1) {
+      roman += "I";
+      n -= 1;
+    }
   }
   return roman
 }
