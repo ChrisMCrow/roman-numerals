087eaa7ecb46ac8103963e6cd2a802e83a8afb7a
Chris Crow and Skye Nguyen <skye@>
2018-08-16 09:31:48 -0700

    pass 5th spec and update ReadMe formatting

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,37 @@ function converter(n) {
   if (n < 1 || n > 3999) {
     return alert("Please enter a number between 1 & 3,999");
   } else {
-    if (n > 10) {
-      var remainderX = n % 10
-      for (i = n; i > 9; i -= 10) {
+    if {
+      
+    } else if (n >= 50) {
+      var remainderL = n % 50;
+      for (i = n; i > 49; i -= 50) {
+        roman += "L";
+      }
+      var remainderX = remainderL % 10;
+      for (i = remainderL; i > 9; i -= 10) {
         roman += "X";
       }
-      var remainderV = remainderX % 5
+      var remainderV = remainderX % 5;
       for (i = remainderX; i > 4; i -= 5) {
         roman += "V";
       }
       for (i = remainderV; i > 0; i--) {
         roman += "I";
       }
-    } else if (n > 5) {
+    } else if (n >= 10) {
+      var remainderX = n % 10;
+      for (i = n; i > 9; i -= 10) {
+        roman += "X";
+      }
+      var remainderV = remainderX % 5;
+      for (i = remainderX; i > 4; i -= 5) {
+        roman += "V";
+      }
+      for (i = remainderV; i > 0; i--) {
+        roman += "I";
+      }
+    } else if (n >= 5) {
       var remainderV = n % 5
       for (i = n; i > 4; i -= 5) {
         roman += "V"
