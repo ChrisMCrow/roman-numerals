8dc0c0f498b5975f642d89968383e092faa69fde
Chris Crow and Skye Nguyen <skye@>
2018-08-16 09:15:06 -0700

    pass 3rd spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,24 @@
 //back-end
 function converter(n) {
-  var Roman = "";
-  if (n < 1 || n > 3,999) {
-    return alert("Please enter a number between 1 & 3,999")
+  var roman = "";
+  if (n < 1 || n > 3999) {
+    return alert("Please enter a number between 1 & 3,999");
   } else {
-  for (i = n; i > 0; i--) {
-    Roman += "I";
+    if (n > 5) {
+      var remainderV = n % 5
+      for (i = n; i > 4; i -= 5) {
+        roman += "V"
+      }
+      for (i = remainderV; i > 0; i--) {
+        roman += "I";
+      }
+    } else {
+      for (i = n; i > 0; i--) {
+        roman += "I";
+      }
     }
   }
-  return Roman;
+  return roman;
 }
 
 
