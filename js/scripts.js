335594988a37a014c324cafaf6351a28180b304d
Chris Crow and Skye Nguyen <skye@>
2018-08-16 09:49:28 -0700

    pass 8th & 9th spec

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,35 @@ function converter(n) {
   if (n < 1 || n > 3999) {
     return alert("Please enter a number between 1 & 3,999");
   } else {
-    if (n >= 500) {
+    if (n >= 1000) {
+      var remainderM = n % 1000;
+      for (i = n; i > 999; i -= 1000) {
+        roman += "M";
+      }
+      var remainderD = remainderM % 500;
+      for (i = remainderM; i > 499; i -= 500) {
+        roman += "D";
+      }
+      var remainderC = remainderD % 100;
+      for (i = remainderD; i > 99; i -= 100) {
+        roman += "C";
+      }
+      var remainderL = remainderC % 50;
+      for (i = remainderC; i > 49; i -= 50) {
+        roman += "L";
+      }
+      var remainderX = remainderL % 10;
+      for (i = remainderL; i > 9; i -= 10) {
+        roman += "X";
+      }
+      var remainderV = remainderX % 5;
+      for (i = remainderX; i > 4; i -= 5) {
+        roman += "V";
+      }
+      for (i = remainderV; i > 0; i--) {
+        roman += "I";
+      }
+    } else if (n >= 500) {
       var remainderD = n % 500;
       for (i = n; i > 499; i -= 500) {
         roman += "D";
@@ -86,10 +114,16 @@ function converter(n) {
       }
     } else {
       for (i = n; i > 0; i--) {
+        if (i === 4) {
+          roman += "IV";
+          break;
+        } else {
         roman += "I";
+        }
       }
     }
   }
+
   return roman;
 }
 
